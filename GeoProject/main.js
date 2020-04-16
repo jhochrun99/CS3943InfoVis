9d2aa86a77580f616718c241f2ba5ba6c827e161
Jess <jhochrun99@gmail.com>
2020-04-15 23:39:34 -0400

    Update main.js
    changed score.csv to DonationsByCountry.csv

--- GeoProject/main.js
+++ GeoProject/main.js
@@ -1,7 +1,7 @@
 // Load the datasets and call the functions to make the visualizations
 
 Promise.all([
-  d3.csv('data/scores.csv', d3.autoType),
+  d3.csv('data/DonationsByCountry.csv', d3.autoType),
   d3.json('data/countries.json'),
 ]).then(([data, geoJSON]) => {
   vis1(data, d3.select('#vis1'));
