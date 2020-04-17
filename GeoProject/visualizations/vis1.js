06b303e817e705227b8ba006d4ba3e956cce6fc9
Jess Hochrun <jh5367@nyu.edu>
2020-04-16 23:43:47 -0400

    Improved x-axis tick marks

--- GeoProject/visualizations/vis1.js
+++ GeoProject/visualizations/vis1.js
@@ -24,9 +24,9 @@ function vis1(data, div) {
 
   // create scales
   
-  const scaleDown = 1000000;
   const x = d3.scaleLinear()
-    .domain([d3.min(data, d => d.net)/scaleDown, d3.max(data, d => d.net)/scaleDown]).nice()
+    .domain(d3.extent(data, d => d.net)).nice()
+    //.domain([d3.min(data, d => d.net), d3.max(data, d => d.net)]).nice()
     .range([0, visWidth]);
 
   const sortedNames = data.sort((a, b) => d3.ascending(a.net, b.net))
@@ -39,7 +39,7 @@ function vis1(data, div) {
 
   // create and add axes
 
-  const xAxis = d3.axisBottom(x);
+  const xAxis = d3.axisBottom(x).tickFormat(d3.format("~s"));
 
   g.append("g")
     .attr("transform", `translate(0, ${visHeight})`)
@@ -50,7 +50,7 @@ function vis1(data, div) {
     .attr("y", 40)
     .attr("fill", "black")
     .attr("text-anchor", "middle")
-    .text("Net Donation (M$, Amount Donated - Received)");
+    .text("Net Donation $ (Amount Donated - Received)");
 
   const yAxis = d3.axisLeft(y);
 
@@ -63,9 +63,9 @@ function vis1(data, div) {
   g.selectAll("rect")
     .data(data)
     .join("rect")
-    .attr("x", d => x(Math.min(0, d.net / scaleDown)))
+    .attr("x", d => x(Math.min(0, d.net)))
     .attr("y", d => y(d.country))
-    .attr("width", d => x(Math.abs(d.net / scaleDown)-140000))
+    .attr("width", d => x(Math.abs(d.net)) - x(0))
     .attr("height", d => y.bandwidth())
     .attr("fill", "steelblue");
             
