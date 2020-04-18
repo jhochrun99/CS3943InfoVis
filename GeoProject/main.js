503b941e3178a4a95e39813a93190dede61ec1d5
Jess Hochrun <jh5367@nyu.edu>
2020-04-17 20:26:37 -0400

    Working on vis2

--- GeoProject/main.js
+++ GeoProject/main.js
@@ -5,5 +5,5 @@ Promise.all([
   d3.json('data/countries.json'),
 ]).then(([data, geoJSON]) => {
   vis1(data, d3.select('#vis1'));
-  vis2(geoJSON, d3.select('#vis2'));
+  vis2(geoJSON, data, d3.select('#vis2'));
 });
--- GeoProject/visualizations/vis2.js
+++ GeoProject/visualizations/vis2.js
@@ -1,8 +1,8 @@
-function vis2(geoJSON, div) {
+function vis2(geoJSON, data, div) {
   const margin = {top: 20, right: 20, bottom: 20, left: 20};
 
-  const visWidth = 700 - margin.left - margin.right;
-  const visHeight = 400 - margin.top - margin.bottom;
+  const visWidth = 775 - margin.left - margin.right;
+  const visHeight = 450 - margin.top - margin.bottom;
 
   const svg = div.append('svg')
       .attr('width', visWidth + margin.left + margin.right)
@@ -16,6 +16,16 @@ function vis2(geoJSON, div) {
 
   const path = d3.geoPath().projection(projection);
 
+  //const allCountries = Object.keys(geoJSON).map(function(key) { return [key, geoJSON[key]]; });
+  //const allCountriesNET = Object.fromEntries(new Map(turnArray[2][1].map(d => [d.properties.SOVEREIGNT, 0])));
+  //const countryDonations = d3.csvParse(await FileAttachment('DonationsByCountry.csv').text(), d3.autoType);
+
+  const countriesOnly = data.map(d => d.country);
+  //const CountryToNet = Object.fromEntries(new Map(countryDonations.map(d => [d.country, d.net])));
+  
+  const color = d3.scaleDiverging(d => d3.interpolateRdBu(d))
+      .domain(d3.extent(data, d => d.net));
+
   g.selectAll('.border')
     .data(geoJSON.features)
     .join('path')
@@ -23,6 +33,10 @@ function vis2(geoJSON, div) {
       .attr('d', path)
       .attr('fill', '#dcdcdc')
       .attr('stroke', 'white');
+
+  g.selectAll('.border')
+    .data(geoJSON.features.filter(d => countriesOnly.includes(d.properties.NAME) || countriesOnly.includes(d.properties.NAME_LONG)))
+      .attr('fill', d => color());
   
   const mapOutline = d3.geoGraticule().outline();
   
